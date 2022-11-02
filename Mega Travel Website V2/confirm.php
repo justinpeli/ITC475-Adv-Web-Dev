c2cf3fb4e848d1045a638624473885810438a943
Justin Pelizzaro <jpelizzaro@gmail.com>
2022-11-02 16:15:43 -0500

    database functionality added

--- Mega Travel Website V2/confirm.php
+++ Mega Travel Website V2/confirm.php
@@ -1,3 +1,37 @@
+<?php
+
+    $firstName = $_POST["firstName"];
+    $lastName = $_POST["lastName"];
+    $phoneNumber = $_POST["phoneNumber"];
+    $email = $_POST["email"];
+    $adultTravelers = $_POST["adultTravelers"];
+    $childrenTravelers = $_POST["kidTravelers"];
+    $selectedLocation = getLocationName($_POST["selectedLocation"]);
+    $originalDate = $_POST["startDate"];
+    $date = formatDate($originalDate);
+    $selectedActivities = getSelectedActivities();
+    $dbServerName = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "mega_travel";
+
+    $conn = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbName);
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $sqlInsert = "INSERT INTO TripFormInfo (id, FirstName, LastName, PhoneNumber, 
+        Email, NumberOfAdults, NumberOfChildren, Destination, TravelDate, 
+        InterestedActivities) VALUES (NULL, '$firstName', '$lastName', '$phoneNumber',
+        '$email', '$adultTravelers', '$childrenTravelers', '$selectedLocation', '$date', '$selectedActivities')";
+
+    if (mysqli_query($conn, $sqlInsert))
+
+    mysqli_close($conn);
+
+?>
+
 <html>
     <head>
         <title>Mega Travel | Form</title>
@@ -12,14 +46,14 @@
         <section style = "padding: 20px">
             <span style = "font-style: italic; color: red">Thank you for submitting your 
                 travel agent contact request! Here is the information you submitted:</span><br><br>
-            Client Name: <span style = "font-style: italic;"><b><?php echo $_POST["firstName"]." ".$_POST["lastName"]; ?></b></span><br><br>
-            Client Phone Number: <span style = "font-style: italic;"><b><?php echo $_POST["phoneNumber"]; ?></b></span><br><br>
-            Client Email: <span style = "font-style: italic;"><b><?php echo $_POST["email"]; ?></b></span><br><br>
-            Number of Adults: <span style = "font-style: italic;"><b><?php echo $_POST["adultTravelers"]; ?></b></span><br><br>
-            Number of children: <span style = "font-style: italic;"><b><?php echo $_POST["kidTravelers"]; ?></b></span><br><br>
-            Destination: <span style = "font-style: italic;"><b><?php echo getLocationName($_POST["selectedLocation"]); ?></b></span><br><br>
-            Travel Dates: <span style = "font-style: italic;"><b><?php echo $_POST["startDate"]; ?></b></span><br><br>
-            Interested Activities: <span style = "font-style: italic;"><b><?php echo getSelectedActivities(); ?></b></span><br><br>
+            Client Name: <span style = "font-style: italic;"><b><?php echo $firstName." ".$lastName; ?></b></span><br><br>
+            Client Phone Number: <span style = "font-style: italic;"><b><?php echo $phoneNumber; ?></b></span><br><br>
+            Client Email: <span style = "font-style: italic;"><b><?php echo $email; ?></b></span><br><br>
+            Number of Adults: <span style = "font-style: italic;"><b><?php echo $adultTravelers; ?></b></span><br><br>
+            Number of children: <span style = "font-style: italic;"><b><?php echo $childrenTravelers ?></b></span><br><br>
+            Destination: <span style = "font-style: italic;"><b><?php echo $selectedLocation; ?></b></span><br><br>
+            Travel Dates: <span style = "font-style: italic;"><b><?php echo $date; ?></b></span><br><br>
+            Interested Activities: <span style = "font-style: italic;"><b><?php echo $selectedActivities; ?></b></span><br><br>
             An agent will be in touch with you soon!
         </section>
         <footer>
@@ -33,22 +67,22 @@
     <?php
         function getLocationName($value){
 
-            $locaitonName = "";
+            $locationName = "";
 
             if($value == "mexico"){
-                $locaitonName = "Cancun, Mexico";
+                $locationName = "Cancun, Mexico";
             }
             else if($value == "maldives"){
-                $locaitonName = "Maldives, South Asia";
+                $locationName = "Maldives, South Asia";
             }
             else if($value == "italy"){
-                $locaitonName = "Venice, Italy";
+                $locationName = "Venice, Italy";
             }
             else if($value == "new zealand"){
-                $locaitonName = "New Zealand";
+                $locationName = "New Zealand";
             }
 
-            return $locaitonName;
+            return $locationName;
         }
 
         function getSelectedActivities(){
@@ -97,6 +131,11 @@
 
             return $activitesString;
 
+        }
+
+        function formatDate($originalDate){
+            $newDate = date("Y-m-d", strtotime($originalDate));
+            return $newDate;
         }
 
     ?>
